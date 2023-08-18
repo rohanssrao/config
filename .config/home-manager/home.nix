857a6e00cc07fc19e89bbfa997637e12a69b496e
Rohan Rao <rsrao@ncsu.edu>
2023-08-18 11:43:19 -0400

    add home-manager

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+
+{
+
+  home.packages = with pkgs; [
+  
+    fish
+    neovim
+      xclip
+      python311Packages.flake8
+    hyfetch
+    gcc
+    fd
+    ripgrep
+    exa
+    bat
+    age
+    lolcat
+    github-copilot-cli
+    any-nix-shell
+    obsidian
+
+    gnomeExtensions.appindicator
+    gnomeExtensions.arcmenu
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.brightness-control-using-ddcutil
+    gnomeExtensions.disable-workspace-switch-animation-for-gnome-40
+    gnomeExtensions.gesture-improvements
+    gnomeExtensions.just-perfection
+    gnomeExtensions.user-themes
+    gnomeExtensions.removable-drive-menu
+    #gnomeExtensions.gsconnect
+
+  ];
+
+  programs.home-manager.enable = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  home.username = "chika";
+  home.homeDirectory = "/var/home/chika";
+
+  home.stateVersion = "23.05"; # Don't change
+
+}
