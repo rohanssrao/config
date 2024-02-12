cf8e424c6ac1adef0900cfb861c87747a172ccee
Rohan Rao <rohanssrao@gmail.com>
2024-02-12 16:33:46 -0500

    Refactor lvim config (3)

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,32 +1,22 @@
--- :PackerSync
 lvim.plugins = {
     { "jcypret/nord-vim-midnight" },
 }
 
 lvim.colorscheme = "nord"
-lvim.format_on_save.enabled = false
 lvim.builtin.autopairs.active = false
 
--- tab indent (2)
-vim.opt.autoindent = true
-vim.cmd("set noexpandtab")
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
 
 vim.opt.timeoutlen = 250
 
--- save with Ctrl+S
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- switch between tabs
-lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
 
 lvim.builtin.lualine.options = {
-	component_separators = '|',
 	section_separators = { left = '', right = '' },
 }
 
 lvim.builtin.lualine.sections = {
-	lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+	lualine_a = { { 'mode' } },
 	lualine_b = { 'filename', 'branch' },
 }
