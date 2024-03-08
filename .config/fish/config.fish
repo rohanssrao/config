c3992e59371cf6ab5c7a7541efc98f10d29d12c1
Rohan Rao <rohanssrao@gmail.com>
2024-03-08 15:44:38 -0500

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,17 @@
-alias vim='lvim'
+command -q lvim && alias vim='lvim'
+command -q eza && alias ls='eza -a --icons'
 alias cp='cp -i'
 alias mv='mv -i'
-alias ls='eza -a --icons'
 alias open='xdg-open 2>/dev/null'
 alias copy='xclip -selection c'
 alias vimf='fd -H -t f . ~ | fzf -i | xargs -ro $EDITOR'
 alias cdf='cd (fd -H -t d . ~ | fzf -i)'
 alias cfg='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias run='NIXPKGS_ALLOW_UNFREE=1 nix run nixpkgs#$argv[1]'
+function run; NIXPKGS_ALLOW_UNFREE=1 nix run nixpkgs#$argv[1]; end
 alias edit='$EDITOR /etc/nixos/configuration.nix'
-alias rebuild='sudo nixos-rebuild switch --flake /etc/nixos#default \
-               && git -C /etc/nixos commit -am (nixos-rebuild list-generations | grep current)'
-
+alias rebuild='git -C /etc/nixos add -A \
+               && sudo nixos-rebuild switch --flake /etc/nixos#default \
+               && git -C /etc/nixos commit -m (nixos-rebuild list-generations | grep current)'
 
 fish_add_path ~/Backups/scripts
 
@@ -19,7 +19,8 @@ set fish_prompt_pwd_dir_length 3
 set fish_greeting
 
 set -x SHELL (which fish)
-set -x EDITOR (which lvim)
+set -x EDITOR (which vi)
+command -q lvim && set -x EDITOR (which lvim)
 set -x SUDO_EDITOR "$EDITOR"
 set -x VISUAL "$EDITOR"
 
@@ -27,5 +28,5 @@ fish_vi_key_bindings
 
 bind -M insert \el 'forward-word'   # alt-L to accept next autocomplete word
 bind -M insert \ej 'down-or-search' # alt-J/K history navigation
-bind -M insert \ek 'up-or-search'
-bind -M insert \cr 'history-pager'  # re-bind history shortcut
+bind -M insert \ek 'up-or-search'   # alt-J/K history navigation
+bind -M insert \cr 'history-pager'  # re-bind history shortcut for vi mode
