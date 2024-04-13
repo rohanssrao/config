f90c09bd9068369a05d6234660752842c351e0c7
Rohan Rao <rohanssrao@gmail.com>
2024-04-13 10:09:01 -0400

    refactor config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,12 @@
-if command -q lvim; alias vim='lvim'; end
-if command -q eza; alias ls='eza -a --icons'; end
 alias cp='cp -i'
 alias mv='mv -i'
-alias vimf='fd -H -t f . ~ | fzf --preview "bat -n --color=always {}" | xargs -ro $EDITOR'
-alias cdf='cd (fd -H -t d . ~ | fzf --preview "ls --color=always {}")'
-alias cfg='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias open='xdg-open 2>/dev/null'
 alias copy='xclip -selection c'
+alias open='xdg-open 2>/dev/null'
+alias cfg='git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias cdf='cd (fd -H -t d . ~ | fzf --preview "ls --color=always {}")'
+alias vimf='fd -H -t f . ~ | fzf --preview "bat -n --theme=Nord --color=always {}" | xargs -ro $EDITOR'
+if command -q lvim; alias vim='lvim'; end
+if command -q eza; alias ls='eza -a --icons'; end
 
 set -x SHELL (command -v fish)
 set -x EDITOR (command -v vi)
@@ -18,10 +18,14 @@ set fish_prompt_pwd_dir_length 3
 set fish_greeting
 
 fish_add_path ~/Backups/scripts
+
 fish_vi_key_bindings
 
-bind -M insert \el 'forward-word'          # alt-l to accept next autocomplete word
-bind -M insert \ej 'down-or-search'        # alt-j/k history navigation
-bind -M insert \ek 'up-or-search'          # alt-j/k history navigation
-bind -M insert \cr 'history-pager'         # re-bind (vi mode)
-bind -M insert \cf 'accept-autosuggestion' # re-bind (vi mode)
+# \e = alt, \c = ctrl
+bind -M insert \el 'forward-word'
+bind -M insert \ej 'down-or-search'
+bind -M insert \ek 'up-or-search'
+bind -M insert \cr 'history-pager'
+bind -M insert \cf 'accept-autosuggestion'
+bind yy fish_clipboard_copy
+bind p fish_clipboard_paste
