c0cfa3b0a1405e6ab2d7e4b6243fc253b8b28e47
Rohan Rao <rohanssrao@gmail.com>
2024-11-30 10:50:30 -0500

    simplify nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,57 +1,26 @@
--- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
--- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
-
--- validate that lazy is available
 if not pcall(require, "lazy") then
-  -- stylua: ignore
   vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
 
 require("lazy").setup({
-  {
-    "AstroNvim/AstroNvim",
-    version = "^4", -- Remove version tracking to elect for nighly AstroNvim
-    import = "astronvim.plugins",
-    opts = { -- AstroNvim options must be set here with the `import` key
-      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
-      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
-      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
-      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
-      update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
-    },
-  },
-  {
-    "AstroNvim/astrocore",
+  { "AstroNvim/AstroNvim", import = "astronvim.plugins" },
+  { "goolord/alpha-nvim", enabled = false },
+  { "akinsho/toggleterm.nvim", lazy = false, opts = { open_mapping = [[<c-\>]] } },
+  { "AstroNvim/astrocore",
     opts = function(_, opts)
       opts.options.opt.relativenumber = false
+      opts.mappings.n.H = { function() require("astrocore.buffer").nav(-vim.v.count1) end }
+      opts.mappings.n.L = { function() require("astrocore.buffer").nav(vim.v.count1) end }
+      opts.mappings.v["<"] = { "<gv" }
+      opts.mappings.v[">"] = { ">gv" }
       return opts
     end
   },
-  { "goolord/alpha-nvim", enabled = false },
-  { "akinsho/toggleterm.nvim", lazy = false, opts = { open_mapping = [[<c-\>]] } },
-
-} --[[@as LazySpec]], {
-  -- Configure any other `lazy.nvim` configuration options here
-  install = { colorscheme = { "astrodark", "habamax" } },
-  ui = { backdrop = 100 },
-  performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
-      },
-    },
-  },
-} --[[@as LazyConfig]])
+})
